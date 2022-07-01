347f5433898910825166107b5b45b0d9a3b6bce6
Aayush Dubey <98901572+aayushdubey-projects@users.noreply.github.com>
2022-07-01 15:37:48 +0530

    Add files via upload

---
+++ Training Day - 5/Task - 1/routes/homeRouter.js
@@ -0,0 +1,14 @@
+var express=require("express");
+var router=express.Router();
+
+router.get("/",(req,res)=>{
+    res.render("index")
+});
+
+router.get("/search",(req,res)=>{
+    res.send(req.query);
+})
+
+
+
+module.exports=router;
